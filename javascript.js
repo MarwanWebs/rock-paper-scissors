8ffd8da9e9510df0ef61e3f7f86fbed520137d87
marwanwebs <libyandoesgt@gmail.com>
2022-12-07 23:18:19 +0200

    an improved version that randomize the computer choice every round.

--- javascript.js
+++ javascript.js
@@ -2,46 +2,49 @@ function getComputerChoice() {
 let choice = ["Rock", "Paper", "Scissors"]
 return randomChoice = choice[Math.floor(Math.random() * choice.length)]
 }
-const ComputerSelection = getComputerChoice();
-console.log(randomChoice)
-
 function playerSelection(){
     let selection = prompt("Rock, Paper, or scissors?")
     let selectionLower = selection.toLowerCase()
     return selectionLower
     
 }
-function playRound(playerSelection, ComputerSelection) {
-        if (playerSelection == "rock"  && ComputerSelection == "Paper") {
+function playRound(playerSelection, getComputerChoice) {
+        if (playerSelection == "rock"  && getComputerChoice == "Paper") {
         return "You lost, Paper Kills Rock.";
     }
-    else if (playerSelection == "paper" && ComputerSelection == "Scissors"){
+    else if (playerSelection == "paper" && getComputerChoice == "Scissors"){
         return "You lost, Scissors Kill Paper";
     }
-    else if (playerSelection == 'scissors'  && ComputerSelection == "Rock"){
+    else if (playerSelection == 'scissors'  && getComputerChoice == "Rock"){
         return "You lost, Rock Kills Scissors";}
-    else if (playerSelection == 'scissors'  && ComputerSelection == "Paper"){
+    else if (playerSelection == 'scissors'  && getComputerChoice == "Paper"){
         return "You Won, Scissors Kill Paper."
     }
-    else if (playerSelection == 'scissors'  && ComputerSelection == "Scissors"){
+    else if (playerSelection == 'scissors'  && getComputerChoice == "Scissors"){
         return "it's a tie";
     }
-    else if (playerSelection == "paper"  && ComputerSelection == "Rock"){
+    else if (playerSelection == "paper"  && getComputerChoice == "Rock"){
         return "You won, Paper beats rock."
     } 
-    else if (playerSelection == "paper" && ComputerSelection == "Paper"){
+    else if (playerSelection == "paper" && getComputerChoice == "Paper"){
         return "it's a tie"
     }
 
-    else if (playerSelection == "rock"   && ComputerSelection == "Scissors"){
+    else if (playerSelection == "rock"   && getComputerChoice == "Scissors"){
         return "You won, Rock beats scissors"
     }
-    else if (playerSelection == "rock"   && ComputerSelection == "Rock"){
+    else if (playerSelection == "rock"   && getComputerChoice == "Rock"){
         return "it's a tie";
     }
     else{
         return "you entered a wrong selection"
     }
     }
-
-console.log(playRound(playerSelection(), ComputerSelection))
+function game (){
+    console.log(playRound(playerSelection(), getComputerChoice()))
+    console.log(playRound(playerSelection(), getComputerChoice()))
+    console.log(playRound(playerSelection(), getComputerChoice()))
+    console.log(playRound(playerSelection(), getComputerChoice()))
+    console.log(playRound(playerSelection(), getComputerChoice()))
+}
+game()
