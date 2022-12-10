8161c8fcac92f13a52d7fa47032470033f02b19c
marwanwebs <libyandoesgt@gmail.com>
2022-12-10 21:44:37 +0200

    added comments

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
+// a function that chooses a random choice for the computer
 function getComputerChoice() {
 let choice = ["Rock", "Paper", "Scissors"]
 return randomChoice = choice[Math.floor(Math.random() * choice.length)]
 }
+// a function That Takes Rock, Paper, Scissors choices from the player. 
 function playerSelection(){
     let selection = prompt("Rock, Paper, or scissors?")
     let selectionLower = selection.toLowerCase()
     return selectionLower
     
 }
+// function upon called plays a round of rock paper scissors
 function playRound(playerSelection, getComputerChoice) {
         if (playerSelection == "rock"  && getComputerChoice == "Paper") {
         return "You lost, Paper Kills Rock.";
@@ -40,6 +43,7 @@ function playRound(playerSelection, getComputerChoice) {
         return "you entered a wrong selection"
     }
     }
+// function once called plays 5 rounds of rock paper scissors
 function game (){
     console.log(playRound(playerSelection(), getComputerChoice()))
     console.log(playRound(playerSelection(), getComputerChoice()))
